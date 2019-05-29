68b1634829de716d06ebe6ee8812415848d9c920
Amr3ss <pravin4094@gmail.com>
2019-05-29 17:20:29 -0400

    Finished linear combination testing

--- mkcols.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-plumed_file = "input.plu"
-traj_file = "md1.xtc"
-bashCommand = "plumed driver --plumed " + plumed_file + " --mf_xtc " + traj_file
-
-process = subprocess.Popen(bashCommand.split())

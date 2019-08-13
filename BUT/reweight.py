5b4b580b696f7fd8745421ad534e1f10ad9a8118
Pavan Ravindra <pravin4094@gmail.com>
2019-08-13 17:30:10 -0400

    made reweight.py (hopefully finished have yet to test on marcc

--- BUT/reweight.py
+++ BUT/reweight.py
@@ -1,58 +1,30 @@
-#!/usr/bin/env python
-"""This reweighting code is based on the algorithm proposed by Tiwary
-and Parrinello, JPCB 2014, 119 (3), 736-742. This is a modified version
-of the reweighting code based on earlier version (v1.0 - 23/04/2015)
-available in GitHub which was originally written by L. Sutto and
-F.L. Gervasio, UCL.
-
-Co-Author: Debabrata Pramanik       pramanik@umd.edu
-Co-Author: Zachary Smith            zsmith7@terpmail.umd.edu """
-
 import os.path
 import argparse
 import numpy as np
 from math import log, exp, ceil
 
-
-
 # Default Arguments
 gamma = 15                    # Biasfactor in well-tempered metadynamics.
 kT = 2.5                      # Temperature times Boltzmann constant.
-fesfilename = "../hills/fes_" # FES file name start.
-numdat = 992                  # Number of FES files.
+fesfilename = "fes_" # FES file name start.
 col_fe = 1                    # Column of free energy.
-datafile = "sb0123"           # COLVAR file name.
-col_rewt = [2,3,5,6]          # COLVAR columns corresponding to RC variables.
-col_bias = [7]                # COLVAR bias column.
+datafile = "../b0-03/BIASED_COLVAR"           # COLVAR file name.
+col_rewt = [2]          # COLVAR columns corresponding to RC variables.
+col_bias = [3]                # COLVAR bias column.
 ngrid = 30                    # Number of grid bins.
 
-
-def weights():
-    # compute reweighting weights from FES data and bias column
-    global kT, numdat, col_bias, weights
-    numcolv = np.shape(colvar)[0]
-    weights = np.zeros(numcolv)
-
-    # go through the CV(t) trajectory
-    i = 0
-    for row in colvar:
-        i += 1
-        indx = int(ceil(float(i)/numcolv*numdat))-1
-        bias = sum([row[j] for j in col_bias])
-        ebias = exp(bias/kT)/ebetac[indx]
-        weights[i-1] = ebias
-
-def ebc():
+def ebc(num):
     global colvar,ebetac
     # File Inputs
-    colvar = np.loadtxt(datafile)
+    tmp = 2500000 * (num + 1) + 1
+    colvar = np.loadtxt(datafile, max_rows=tmp)
 
     # Calculating c(t):
     # calculates ebetac = exp(beta c(t)), using eq. 12 in eq. 3 in the JPCB paper
     #
     ebetac = []
 
-    for i in range(numdat):
+    for i in range(num + 1):
         # set appropriate format for FES file names, NB: i starts from 0
         fname = '%s%d.dat' % (fesfilename,i)
 
@@ -64,21 +36,31 @@ def ebc():
             s2 += exp(exponent/gamma)
         ebetac += s1 / s2,
 
-def load():
+def weights(num):
+    # compute reweighting weights from FES data and bias column
+    global kT, col_bias, weight
+    numcolv = np.shape(colvar)[0]
+    weight = np.zeros(numcolv)
+
+    # go through the CV(t) trajectory
+    i = 0
+    for row in colvar:
+        i += 1
+        indx = int(ceil(float(i)/numcolv*(num + 1)))-1
+        bias = sum([row[j] for j in col_bias])
+        ebias = exp(bias/kT)/ebetac[indx]
+        weight[i-1] = ebias
+
+def load(num):
     # Loads given files. Runs on import to prevent redundant loading, note this increases import time.
-    ebc()
-    weights()
-
-
-load()
-
-
+    ebc(num)
+    weights(num)
 
 def reweight(sparse=False,size=30,data=None):
     # Reweighting biased MD trajectory to unbiased probabilty along a given RC.
     # By default (sparse=False) bins on the edge of the range with probabilities lower
     # than 1/N where N is number of data points will be removed.
-    global kT, fesfilename, numdat, col_fe, datafile, col_rewt, numrewt, col_bias, ngrid, s_min, s_max
+    global kT, fesfilename, col_fe, datafile, col_rewt, numrewt, col_bias, ngrid, s_min, s_max
 
     rc = [1]
 
@@ -95,7 +77,7 @@ def reweight(sparse=False,size=30,data=None):
     # initialize square array numrewt-dimensional
 
 
-    hist = np.histogram(rc_space,size,weights=weights)[0]
+    hist = np.histogram(rc_space,size,weights=weight)[0]
     pnorm = hist/np.sum(hist)
 
 
@@ -111,3 +93,11 @@ def reweight(sparse=False,size=30,data=None):
             trimmed = pnorm[np.min(trim):np.max(trim)+1]
         return trimmed
     return pnorm
+
+vals= []
+for i in range(1000):
+    load(i)
+    plz = reweight()
+    vals.append(plz)
+
+np.save("RESULTS", vals)

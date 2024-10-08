82440cd39b7543ff7b1d8a365843d51d5ad60b5a
Da Teng <tengda1997@gmail.com>
2024-10-08 19:06:22 -0400

    rename the constant in tests

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import amino
 import numpy as np
 
+# Load the test data
 dat = np.loadtxt("data/BOUND_COLVAR").T
+
+# Initialize the list of OPs
 ops = [amino.OrderParameter(f"test_OP_{i}", d) for i, d in enumerate(dat)]
+
+# Intialize the distance matrix
 memo = amino.DistanceMatrix(50, 0.02, "epanechnikov")
 memo.initialize_distances(ops)
+
+# convenience variables
 OP_1 = ops[0]
 OP_2 = ops[1]
+dist_1_2 = 0.5353485087893664
 
 
 def test_load_data():
@@ -31,7 +39,7 @@ def test_distance_2():
 
     dist = memo.distance(OP_1, OP_2)
 
-    assert np.isclose(dist, 0.5353485087893664)
+    assert np.isclose(dist, dist_1_2)
 
 
 def test_distance_3():
@@ -69,7 +77,7 @@ def test_add_op_2():
     dis_matrix = np.array(mat.matrix)
 
     assert np.isclose(dis_matrix[1,0], dis_matrix[0,1])
-    assert np.isclose(dis_matrix[1,0], 0.5353485087893664)
+    assert np.isclose(dis_matrix[1,0], dist_1_2)
     assert np.isclose(dis_matrix[0,0], 0)
     assert np.isclose(dis_matrix[1,1], 0)
 
@@ -95,9 +103,9 @@ def test_add_op_4():
 
     dis_matrix = np.array(mat.matrix)
 
-    assert np.allclose(dis_matrix, [[0, 0.5353485087893664], [0.5353485087893664, 0]])
+    assert np.allclose(dis_matrix, [[0, dist_1_2], [dist_1_2, 0]])
 
 def test_initialization():
 
-    assert np.isclose(memo._distance_kernel(0, 1), 0.5353485087893664)
-    assert np.isclose(memo.distance(ops[0], ops[1]), 0.5353485087893664)
+    assert np.isclose(memo._distance_kernel(0, 1), dist_1_2)
+    assert np.isclose(memo.distance(ops[0], ops[1]), dist_1_2)
